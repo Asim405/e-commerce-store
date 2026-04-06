c90d1ac18de360d0062121fd8a1d59179fea7e79
Asim Sultan <asimsultan405@gmail.com>
2026-04-06 22:07:13 +0500

    favicon inserted but not finalized

--- index.php
+++ index.php
@@ -11,6 +11,10 @@ $featured_products = getFeaturedProducts($pdo, 4);
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Store - Welcome</title>
+    
+    <!-- <link rel="icon" type="image/x-icon" href="assets/images/favicon.ico"> -->
+    <link rel="icon" type="image/x-icon" href="assets/images/favicon4.png">
+
     <link rel="stylesheet" href="assets/css/style.css">
     <link href="https://fonts.googleapis.com/css2?family=Playfair+Display:wght@700;900&family=DM+Sans:wght@300;400;500;600&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css">
